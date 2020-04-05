092227e5fcfc9e9ea1b0a0d9474f41c82b1cd675
Adrian Orcik <adrian.orcik@gmail.com>
2020-04-05 15:52:24 +0300

    implement external logger

--- ugine/src/ugine/log.cc
+++ ugine/src/ugine/log.cc
@@ -1,34 +1,51 @@
 #include "uepch.h"
 
 #include "log.h"
-
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Ugine {
 
 	std::shared_ptr<spdlog::logger> Log::core_logger_;
+	std::shared_ptr<spdlog::logger> Log::core_export_logger_;
+
 	std::shared_ptr<spdlog::logger> Log::client_logger_;
+	std::shared_ptr<spdlog::logger> Log::client_export_logger_;
 
 	Log::Log()
 	{
 		Log::core_logger_ = std::shared_ptr<spdlog::logger>();
+		Log::core_export_logger_ = std::shared_ptr<spdlog::logger>();
+
 		Log::client_logger_ = std::shared_ptr<spdlog::logger>();
+		Log::client_export_logger_ = std::shared_ptr<spdlog::logger>();
 	}
 
 	Log::~Log()
 	{
 		delete &core_logger_;
 		delete &client_logger_;
+		delete &core_export_logger_;
+		delete &client_export_logger_;
 	}
 
 	void Log::Init()
 	{
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
+		core_export_logger_ = spdlog::basic_logger_mt("CORE_LOG", "logs/core_logs.txt");
+		core_export_logger_->set_level(spdlog::level::trace);
+		core_export_logger_->trace("START_SESSION------------------------");
+
 		core_logger_ = spdlog::stderr_color_mt("CORE");
 		core_logger_->set_level(spdlog::level::trace);
 
+		client_export_logger_ = spdlog::basic_logger_mt("APP_LOG", "logs/app_logs.txt");
+		client_export_logger_->set_level(spdlog::level::trace);
+		client_export_logger_->trace("START_SESSION------------------------");
+
 		client_logger_ = spdlog::stderr_color_mt("APP");
 		client_logger_->set_level(spdlog::level::trace);
+
 	}
 }
--- ugine/src/ugine/log.h
+++ ugine/src/ugine/log.h
@@ -3,6 +3,7 @@
 #include "core.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/fmt/ostr.h"
+#include <fstream>
 
 namespace Ugine
 {
@@ -13,27 +14,60 @@ namespace Ugine
 		~Log();
 		static void Init();
 
+		inline static std::shared_ptr<spdlog::logger>& GetCoreExportLogger() { return core_export_logger_; }
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return core_logger_; }
+
+		inline static std::shared_ptr<spdlog::logger>& GetClientExportLogger() { return client_export_logger_; }
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return client_logger_; }
 	private:
+		static std::shared_ptr<spdlog::logger> core_export_logger_;
 		static std::shared_ptr<spdlog::logger> core_logger_;
+
+		static std::shared_ptr<spdlog::logger> client_export_logger_;
 		static std::shared_ptr<spdlog::logger> client_logger_;
 	};
 }
 
+// todo: add more specific macros
+
 // core log macros
-#define CORE_LOG_TRACE(...)	::Ugine::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define CORE_LOG_INFO(...)	::Ugine::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define CORE_LOG_WARN(...)	::Ugine::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define CORE_LOG_ERROR(...)	::Ugine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define CORE_LOG_FATAL(...)	::Ugine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define CORE_LOG_TRACE(...){ ::Ugine::Log::GetCoreLogger()->trace(__VA_ARGS__);			\
+							 ::Ugine::Log::GetCoreExportLogger()->trace(__VA_ARGS__);	\
+}
+
+#define CORE_LOG_INFO(...){	 ::Ugine::Log::GetCoreLogger()->info(__VA_ARGS__);			\
+							 ::Ugine::Log::GetCoreExportLogger()->info(__VA_ARGS__);	\
+}
+
+#define CORE_LOG_WARN(...){	::Ugine::Log::GetCoreLogger()->warn(__VA_ARGS__);		\
+							::Ugine::Log::GetCoreExportLogger()->warn(__VA_ARGS__);	\
+}
+
+#define CORE_LOG_ERROR(...){	::Ugine::Log::GetCoreLogger()->error(__VA_ARGS__);		\
+								::Ugine::Log::GetCoreExportLogger()->error(__VA_ARGS__);	\
+}
+
+#define CORE_LOG_FATAL(...){	::Ugine::Log::GetCoreLogger()->fatal(__VA_ARGS__);			\
+								::Ugine::Log::GetCoreExportLogger()->error(__VA_ARGS__);	\
+}
 
 // client log macros
-#define LOG_TRACE(...)	::Ugine::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define LOG_INFO(...)	::Ugine::Log::GetClientLogger()->info(__VA_ARGS__)
-#define LOG_WARN(...)	::Ugine::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...)	::Ugine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define LOG_FATAL(...)	::Ugine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define LOG_TRACE(...){ ::Ugine::Log::GetClientLogger()->trace(__VA_ARGS__);			\
+						 ::Ugine::Log::GetClientExportLogger()->trace(__VA_ARGS__);	\
+}
 
-#pragma once
+#define LOG_INFO(...){	 ::Ugine::Log::GetClientLogger()->info(__VA_ARGS__);			\
+						 ::Ugine::Log::GetClientExportLogger()->info(__VA_ARGS__);	\
+}
 
+#define LOG_WARN(...){	::Ugine::Log::GetClientLogger()->warn(__VA_ARGS__);		\
+						::Ugine::Log::GetClientExportLogger()->warn(__VA_ARGS__);	\
+}
+
+#define LOG_ERROR(...){	::Ugine::Log::GetClientLogger()->error(__VA_ARGS__);		\
+						::Ugine::Log::GetClientExportLogger()->error(__VA_ARGS__);	\
+}
+
+#define LOG_FATAL(...){	::Ugine::Log::GetClientLogger()->fatal(__VA_ARGS__);			\
+						::Ugine::Log::GetClientExportLogger()->error(__VA_ARGS__);	\
+}
